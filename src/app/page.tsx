beb9c6d61b7f98e27909b8fec8fcc1c3e7a1905e
NeilRow56 <neilrowland56@gmail.com>
2024-04-05 06:13:00 +0100

    Added JobListItem and Badge components. Home page updated to map through and display jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,30 @@
-export default function Home() {
+import JobListItem from '@/components/jobs/JobListItem'
+import { db } from '@/lib/db'
+
+export default async function Home() {
+  const jobs = await db.job.findMany({
+    where: {
+      approved: true,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  })
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2 className="text-primary">Home Page</h2>
+    <main className="m-auto my-10 max-w-5xl space-y-10 px-3">
+      <div className="space-y-5 text-center">
+        <h1 className="text-4xl font-extrabold tracking-tight text-primary lg:text-5xl">
+          Developer jobs
+        </h1>
+        <p className=" text-primary">Find your dream job.</p>
+      </div>
+      <section>
+        <div className="space-y-4">
+          {jobs.map((job) => (
+            <JobListItem job={job} key={job.id} />
+          ))}
+        </div>
+      </section>
     </main>
   )
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,17 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx'
+import { formatDistanceToNowStrict } from 'date-fns'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function formatMoney(amount: number) {
+  return new Intl.NumberFormat('en-GB', {
+    style: 'currency',
+    currency: 'GBP',
+  }).format(amount)
+}
+export function relativeDate(from: Date) {
+  return formatDistanceToNowStrict(from, { addSuffix: true })
 }
